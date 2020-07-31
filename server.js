70f27ce2cc70d35e224178c72059091f66c939e0
alfredo gonzalez <alfredo.gon195@gmail.com>
2020-07-30 21:26:03 -0700

    set up server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express() 
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    //get response and take user to room
+    res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    //get room ID from the url
+    res.render('room', { roomId: req.params.room})
+})
+
+server.listen(3003)
